891e29502e123179c68f12b5edf929f22ae5cd68
aleksandar02 <aca.milic02@gmail.com>
2019-05-05 01:09:54 +0200

    First official commit.

---
+++ js/active.js
@@ -0,0 +1,210 @@
+(function ($) {
+    'use strict';
+
+    var alime_window = $(window);
+
+    // ****************************
+    // Preloader Active Code
+    // ****************************
+
+    alime_window.on('load', function () {
+        $('#preloader').fadeOut('1000', function () {
+            $(this).remove();
+        });
+    });
+
+    // ****************************
+    // ClassyNav Active Code
+    // ****************************
+
+    if ($.fn.classyNav) {
+        $('#alimeNav').classyNav();
+    }
+
+    // *********************************
+    // Welcome Slides Active Code
+    // *********************************
+
+    if ($.fn.owlCarousel) {
+        var welcomeSlider = $('.welcome-slides');
+        welcomeSlider.owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            autoplayTimeout: 10000,
+            nav: true,
+            navText: [('<i class="fas fa-angle-left"></i>'), ('<i class="fas fa-angle-right"></i>')]
+        })
+
+        welcomeSlider.on('translate.owl.carousel', function () {
+            var layer = $("[data-animation]");
+            layer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+
+        welcomeSlider.on('translated.owl.carousel', function () {
+            var layer = welcomeSlider.find('.owl-item.active').find("[data-animation]");
+            layer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+    }
+
+    // ************************************
+    //Instragram Slides Active Code
+    // ************************************
+
+    if ($.fn.owlCarousel) {
+        var instagramFeedSlider = $('.instragram-feed-area');
+        instagramFeedSlider.owlCarousel({
+            items: 6,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            autoplayTimeout: 3000,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                576: {
+                    items: 3
+                },
+                768: {
+                    items: 4
+                },
+                992: {
+                    items: 5
+                },
+                1200: {
+                    items: 6
+                }
+            }
+        })
+    }
+
+    // *********************************
+    // Masonary Gallery Active Code
+    // *********************************
+
+    if ($.fn.imagesLoaded) {
+        $('.alime-portfolio').imagesLoaded(function () {
+            // filter items on button click
+            $('.portfolio-menu').on('click', 'button', function () {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            // init Isotope
+            var $grid = $('.alime-portfolio').isotope({
+                itemSelector: '.single_gallery_item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.single_gallery_item'
+                }
+            });
+        });
+    }
+
+    // ***********************************
+    // Portfolio Button Active Code
+    // ***********************************
+    
+    $('.portfolio-menu button.btn').on('click', function () {
+        $('.portfolio-menu button.btn').removeClass('active');
+        $(this).addClass('active');
+    })
+
+    // ********************************
+    // Search Button Active Code
+    // ********************************
+    $('.search-btn').on('click', function () {
+        $('.search-form').toggleClass('search-form-active');
+    })
+
+    // ************************
+    // 8.0 Stick Active Code
+    // ************************
+
+    alime_window.on('scroll', function () {
+        if (alime_window.scrollTop() > 0) {
+            $('.main-header-area').addClass('sticky');
+        } else {
+            $('.main-header-area').removeClass('sticky');
+        }
+    });
+
+    // *********************************
+    // Magnific Popup Active Code
+    // *********************************
+    if ($.fn.magnificPopup) {
+        $('.video-play-btn').magnificPopup({
+            type: 'iframe'
+        });
+        $('.portfolio-img').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true,
+                preload: [0, 2],
+                navigateByImgClick: true,
+                tPrev: 'Previous',
+                tNext: 'Next'
+            }
+        });
+    }
+
+    // **************************
+    // Tooltip Active Code
+    // **************************
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+
+    // ***********************
+    // WOW Active Code
+    // ***********************
+    if (alime_window.width() > 767) {
+        new WOW().init();
+    }
+
+    // ****************************
+    // Jarallax Active Code
+    // ****************************
+    if ($.fn.jarallax) {
+        $('.jarallax').jarallax({
+            speed: 0.5
+        });
+    }
+
+    // ****************************
+    // Scrollup Active Code
+    // ****************************
+    if ($.fn.scrollUp) {
+        alime_window.scrollUp({
+            scrollSpeed: 1000,
+            scrollText: '<i class="fas fa-angle-up"></i>'
+        });
+    }
+
+    // *********************************
+    // Prevent Default 'a' Click
+    // *********************************
+    $('a[href="#"]').on('click', function ($) {
+        $.preventDefault();
+    });
+
+})(jQuery);
